ca277248d1bfa1093c5857d1cb50c693b99c2588
Oron Ben Zvi <oronbz@gmail.com>
2023-01-23 15:19:46 +0200

    Initial Commit

---
+++ TCAGithub/TCAGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAGithubApp.swift
+//  TCAGithub
+//
+//  Created by Oron Ben Zvi on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
