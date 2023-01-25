1fe1462d4805e30416f2fdf540a9784b023a0ed5
Oron Ben Zvi <oronbz@gmail.com>
2023-01-25 22:00:43 +0200

    Add firebase to project

---
+++ TCAGithub/Models/Comment.swift
@@ -0,0 +1,13 @@
+//
+//  Comment.swift
+//  TCAGithub
+//
+//  Created by Oron Ben Zvi on 25/01/2023.
+//
+
+import Foundation
+
+struct Comment: Equatable, Identifiable {
+    let id: String
+    let content: String
+}
